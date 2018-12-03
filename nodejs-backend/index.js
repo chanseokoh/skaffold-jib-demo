5382287c36d50cd53afd0cfc4b659cd41620fa65
Chanseok Oh <chanseok@google.com>
2018-12-02 23:41:19 -0500

    Add console log to nodejs-backend

--- nodejs-backend/index.js
+++ nodejs-backend/index.js
@@ -2,6 +2,9 @@ const express = require('express')
 const app = express()
 const port = 3000
 
-app.get('/', (req, res) => res.send('Greetings from Node.js!'))
+app.get('/', (req, res) => {
+  console.log('nodejs: got a request to greet');
+  res.send('Greetings from Node.js!');
+})
 
 app.listen(port, () => console.log(`Greetings app listening on port ${port}!`))
